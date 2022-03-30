a52da18020f8b9bfdbfb810657754ae358f58e51
Jonathan Pierre <JonWPierre@gmail.com>
2022-03-30 14:30:25 -0400

    does not need cors extension anymore

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ console.log("Js is running");
 const APIKEY = 5124188170937473;
 let hero="naruto";
 $.ajax({
-    url: `https://superheroapi.com/api/5124188170937473/search/naruto`,
-}).then(data => console.log(data)).catch(error => console.log(error));
+    url: `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/5124188170937473/search/naruto`,
+}).then(data =>{ 
+    console.log(data);
+
+}).catch(error => console.log(error));
 //https://superheroapi.com/api/5124188170937473/search/naruto
